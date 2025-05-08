192e2defe4deda8642041dbc5392b6654a6d4c27
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-05-08 15:59:54 +0200

    refactor Todo List assignment

--- Program.cs
+++ Program.cs
@@ -34,7 +34,6 @@ void MainMenu()
             PrintSelectedOption("Exit");
             break;
         default:
-
             Console.WriteLine("Invalid choice. Please input s, a, r or e");
             MainMenu();
             break;
@@ -42,79 +41,99 @@ void MainMenu()
 }
 var mainList = new List<string> { };
 MainMenu();
-void PrintSelectedOption(string selectedOption)
-{
-    Console.WriteLine("Selected option: " + selectedOption);
-}
+
 void PrintAllToDo()
 {
     if (mainList.Count == 0)
     {
-        Console.WriteLine("No TODOs have been added yet.");
+        ShowNoToDoMessage();
+        return;
     }
-    else
+
+    Console.WriteLine("Here is what you have on your main List: ");
+    for (var i = 0; i < mainList.Count; i++)
     {
-        Console.WriteLine("Here is what you have on your main List: ");
-        for (var i = 0; i < mainList.Count; i++)
-        {
-            Console.WriteLine((i + 1) + ". " + mainList[i]);
-        }
+        Console.WriteLine((i + 1) + ". " + mainList[i]);
     }
 }
-void AddToDo()
+void AddToDo() // high-level method
 {
-    bool isValidDescription = false;
-    while (!isValidDescription)
+    //bool isValidDescription = false;
+    string enteredDescription;
+    do
     {
-        Console.WriteLine("Enter the TODO description:");
-        string enteredDescription = Console.ReadLine();
-
-        if (enteredDescription == "") // first let's check program for exceptions (and throw that exception)
         {
-            Console.WriteLine("The description cannot be empty.");
+            Console.WriteLine("Enter the TODO description:");
+            enteredDescription = Console.ReadLine();
         }
-        else if (mainList.Contains(enteredDescription))
-        {
-            Console.WriteLine("The description must be unique.");
-        }
-        else
-        {
-            isValidDescription = true;
-            mainList.Add(enteredDescription);
-            Console.WriteLine("TODO successfully added: " + enteredDescription);
-        }
-    }
+    } while (!IsDescriptionValid(enteredDescription));
+    mainList.Add(enteredDescription);
+    // A WIADOMOSC O DODANIU TODO?
 }
 void RemoveToDo()
 {
     if (mainList.Count == 0)
     {
-        Console.WriteLine("No TODOs have been added yet.");
+        ShowNoToDoMessage();
         return;
     }
-    bool isIndexValid = false;
-    while (!isIndexValid)
-    { 
-    Console.WriteLine("Select the index of the TODO you want to remove:");
-        PrintAllToDo();
-    string enteredIndex = Console.ReadLine();
-         if (enteredIndex == "")
+    int index;
+    do
+    {
         {
-            Console.WriteLine("Selected index cannot be empty.");
-            continue;
-        }
-        // check if user provided number in input
-    if (int.TryParse(enteredIndex, out int index) && index >= 1 && index <= mainList.Count)
-    {
-            var indexOfTodo = index - 1;
-     var todoToBeRemoved = mainList[indexOfTodo];
-            mainList.RemoveAt(indexOfTodo);
-            isIndexValid = true;
-            Console.WriteLine("TODO successfully removed: " + todoToBeRemoved);
-        }
-       else
-    {
-        Console.WriteLine("The given index is not valid.");
-    }
+            Console.WriteLine("Select the index of the TODO you want to remove:");
+            PrintAllToDo();
+        } 
+    } while (!TryReadIndex(out index));
+        RemoveToDoAtIndex(index - 1);
 }
+
+// helper methods, less important, go in the end of file; more important methods go at the top
+bool IsDescriptionValid(string enteredDescription) // low-lvl method
+{
+    if (enteredDescription == "") // first let's check program for exceptions (and throw that exception)
+    {
+        Console.WriteLine("The description cannot be empty.");
+        return false;
+    }
+    if (mainList.Contains(enteredDescription))
+    {
+        Console.WriteLine("The description must be unique.");
+        return false;
+    }
+    return true;
+}
+bool TryReadIndex(out int index)
+{
+    string enteredIndex = Console.ReadLine();
+    if (enteredIndex == "")
+    {
+        index = 0;
+        Console.WriteLine("Selected index cannot be empty.");
+        return false;
+    }
+    // check if user provided number in input
+    if (int.TryParse(enteredIndex, out index) &&
+        index >= 1 &&
+        index <= mainList.Count)
+    {
+        return true;
+    }
+
+    Console.WriteLine("The given index is not valid.");
+    return false;
+}
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
+void ShowNoToDoMessage()
+{
+    Console.WriteLine("No TODOs have been added yet.");
+}
+void RemoveToDoAtIndex(int index)
+{
+    var todoToBeRemoved = mainList[index];
+    mainList.RemoveAt(index);
+    Console.WriteLine("TODO successfully removed: " + todoToBeRemoved);
 }
