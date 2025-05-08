865dda3bdec4f01a60d2449ddb9b1ee40cea64f0
Ludmila Wiorek <ludmila.wiorek@gmail.com>
2025-05-08 13:20:58 +0200

    modify function remove todo

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("Hello, C#!");
 void MainMenu()
 {
+    Console.WriteLine();
     Console.WriteLine("What do you want to do?");
     Console.WriteLine("[S]ee all TODOs!");
     Console.WriteLine("[A]dd a TODO");
@@ -91,33 +92,29 @@ void RemoveToDo()
         Console.WriteLine("No TODOs have been added yet.");
         return;
     }
+    bool isIndexValid = false;
+    while (!isIndexValid)
+    { 
     Console.WriteLine("Select the index of the TODO you want to remove:");
+        PrintAllToDo();
     string enteredIndex = Console.ReadLine();
-
-    // check if user provided number in input
-    int index;
-    if (int.TryParse(enteredIndex, out index))
-    {
-        if (index > mainList.Count || index <= 0)
+         if (enteredIndex == "")
         {
-            Console.WriteLine("The given index is not valid.");
-            RemoveToDo();
+            Console.WriteLine("Selected index cannot be empty.");
+            continue;
         }
-        else if (index <= mainList.Count)
-        {
-            Console.WriteLine("User input is correct, can proceed with removing from list");
-            Console.WriteLine($"TODO removed: {index}. {mainList[index - 1]}");
-            mainList.RemoveAt(index - 1);
-        }
-    }
-    else if (enteredIndex == "")
+        // check if user provided number in input
+    if (int.TryParse(enteredIndex, out int index) && index >= 1 && index <= mainList.Count)
     {
-        Console.WriteLine("Selected index cannot be empty.");
-        RemoveToDo();
-    }
-    else
+            var indexOfTodo = index - 1;
+     var todoToBeRemoved = mainList[indexOfTodo];
+            mainList.RemoveAt(indexOfTodo);
+            isIndexValid = true;
+            Console.WriteLine("TODO successfully removed: " + todoToBeRemoved);
+        }
+       else
     {
         Console.WriteLine("The given index is not valid.");
-        RemoveToDo();
     }
+}
 }
